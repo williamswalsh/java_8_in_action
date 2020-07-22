f75ed9e5f24c648432376e96999ff7d5e5644a8d
William Walsh <william.walsh@digitary.net>
2020-07-22 07:30:30 +0100

    Add summing methods with performance measuring code

---
+++ src/ie/williamswalsh/NumberOfCores.java
@@ -0,0 +1,8 @@
+package ie.williamswalsh;
+
+public class NumberOfCores {
+    public static void main(String[] args) {
+        System.out.println("Number of Cores: " + Runtime.getRuntime().availableProcessors());
+    }
+}
+
---
+++ src/ie/williamswalsh/testing_performance/Testing.java
@@ -0,0 +1,70 @@
+package ie.williamswalsh.testing_performance;
+
+import java.util.function.Function;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+public class Testing {
+
+    // Old - 3 msecs
+    // NO AutoBoxing time
+    // For loop works at much lower level(not a large startup time)
+    public static long iterativeSum(long n) {
+        long result = 0;
+        for (long l = 1L; l <= n; l++) {
+            result += l;
+        }
+        return result;
+    }
+
+    // Java8 - 99 msecs
+    // AutoBoxing time
+    // Larger startup fee
+    public static long sequentialSum(long n) {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(n)
+                .reduce(0L, Long::sum);
+    }
+
+    // Java8 multicore - 130 msecs
+    // AutoBoxing time
+    // Larger startup fee
+    public static long parallelSum(long n) {
+        return Stream.iterate(1L, i -> i + 1)
+                .limit(n)
+                .parallel()
+                .reduce(0L, Long::sum);
+    }
+
+    public static long rangeClosedSum(long n) {
+        return LongStream.rangeClosed(1, n)
+                .reduce(0L, Long::sum);
+    }
+
+    public static long rangeClosedSumParallel(long n) {
+        return LongStream.rangeClosed(1, n)
+                .parallel()
+                .reduce(0L, Long::sum);
+    }
+
+    // Testing Harness
+    public static long measureAdderPerf(Function<Long, Long> adder, long n) {
+        long fastest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            long sum = adder.apply(n);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+            if (duration < fastest) fastest = duration;
+        }
+        return fastest;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Iterative Sum function duration(milliseconds): " + measureAdderPerf(Testing::iterativeSum, 10_000_000));
+        System.out.println("Sequential Sum function duration(milliseconds): " + measureAdderPerf(Testing::sequentialSum, 10_000_000));
+        System.out.println("Parallel Sum function duration(milliseconds): " + measureAdderPerf(Testing::parallelSum, 10_000_000));
+        System.out.println("Range Closed Sum Unboxed function duration(milliseconds): " + measureAdderPerf(Testing::rangeClosedSum, 10_000_000));
+        System.out.println("Parallel Range Closed Sum Unboxed duration(milliseconds): " + measureAdderPerf(Testing::rangeClosedSumParallel, 10_000_000));
+    }
+
+}
